66ca61a4eb0cce05da644ba532d2e5be0b97c6d7
佐々木勇星 <>
2020-01-07 08:08:56 +0900

    Add ClearScene Files

---
+++ Classes/ClearScene.cpp
@@ -0,0 +1,11 @@
+//
+//  ClearScene.cpp
+//  Pachikuri-mobile
+//
+//  Created by 佐々木勇星 on 2020/01/07.
+//
+
+#include "ClearScene.hpp"
+
+USING_NS_CC;
+
---
+++ Classes/ClearScene.hpp
@@ -0,0 +1,23 @@
+//
+//  ClearScene.hpp
+//  Pachikuri-mobile
+//
+//  Created by 佐々木勇星 on 2020/01/07.
+//
+
+#ifndef ClearScene_hpp
+#define ClearScene_hpp
+
+#include <stdio.h>
+#include <cocos2d.h>
+
+class ClearScene : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+    
+    virtual bool init();
+    CREATE_FUNC(ClearScene);
+};
+
+#endif /* ClearScene_hpp */
