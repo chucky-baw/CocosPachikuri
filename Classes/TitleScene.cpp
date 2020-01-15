5afa6dfccc15c5b125a7dcfd420b00f956702aaa
佐々木勇星 <y_sasaki@vrlab.esys.tsukuba.ac.jp>
2020-01-15 10:56:37 +0900

    Add : StartLabel
    "Tap to Start"ラベルを追加

--- Classes/TitleScene.cpp
+++ Classes/TitleScene.cpp
@@ -26,11 +26,17 @@ bool TitleScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     auto origin = Director::getInstance()->getVisibleOrigin();
     
+    auto startLabel = Label::createWithSystemFont("Tap To Start", "/Users/sasakiyusei/Documents/cocos/CocosPachikuri/Resources/fonts/arial.ttf", 14);
+    startLabel->setAnchorPoint(Vec2(0.5, 0.5));
+    startLabel->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2 + 25));
+    this->addChild(startLabel, 1);
+    
     auto bg = Sprite::create("/Users/sasakiyusei/Documents/cocos/CocosPachikuri/Resources/TitleScene.png");
     bg->setAnchorPoint(Vec2(0.5, 0.5));
     bg->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
     bg->setScale(2.0f, 2.0f);
-    this->addChild(bg, 1);
+    this->addChild(bg);
     
     return true;
+
 }
